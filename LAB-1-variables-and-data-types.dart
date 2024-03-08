f2280844d8b36f7556dac5aece67002fe235f0dd
kalkidanteklay <150287564+kalkidanteklay@users.noreply.github.com>
2024-03-08 16:49:29 +0300

    Add files via upload

---
+++ LAB-1-variables-and-data-types.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = 'Kalkidan';
+  int age = 20;
+  String favoriteColor = 'pink';
+
+  print('My name is $name. I am $age years old. My favourite colour is $favoriteColor');
+  
+}
+
+
